46e49a5aebb22984abe99599f73043c2cc625029
Mariah <mariahcasciola@gmail.com>
2024-03-15 08:41:26 -0500

    Wrote goals in README.md and created recycle bin page

--- src/RecycleBin.js
+++ src/RecycleBin.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-function RecycleBin(){
-return <>
-
-</>
-} 
+function RecycleBin() {
+  return (
+    <>
+      <div className="">
+        <h1 className="h1">Recycle Bin</h1>
+        <ul class="list-group">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+          <li class="list-group-item">A fourth item</li>
+          <li class="list-group-item">And a fifth one</li>
+        </ul>
+      </div>
+    </>
+  );
+}
 
 export default RecycleBin;
