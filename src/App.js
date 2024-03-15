2196832efd227f3ac7eb96f38000ac7ba39f99ca
Mariah <mariahcasciola@gmail.com>
2024-03-14 22:39:13 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import RecycleBin from './RecycleBin';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <RecycleBin/>
     </div>
   );
 }
---
+++ src/RecycleBin.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function RecycleBin(){
+return <>
+
+</>
+} 
+
+export default RecycleBin;
