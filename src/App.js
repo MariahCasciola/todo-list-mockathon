973d20eea05d960ad0347366f209e22b0d97722b
Jose Padilla <jose.apadilla99@gmail.com>
2024-03-14 22:50:35 -0500

    boilerplate for addtask bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import RecycleBin from './RecycleBin';
+import logo from "./logo.svg";
+import "./App.css";
+import RecycleBin from "./RecycleBin";
+import { useState } from "react";
+import List from "./components/List/List";
+import AddTask from "./components/List/AddTask";
 
 function App() {
-  const [todos, setTodos] = useState(["write", "read", "sleep"])
+  const [todos, setTodos] = useState(["write", "read", "sleep", "eat"]);
   return (
     <div className="App">
       <header className="App-header">
-        <List todos={todos}/>
+        <List todos={todos} />
+        <AddTask />
       </header>
-      <RecycleBin/>
+      <RecycleBin />
     </div>
   );
 }
---
+++ src/components/List/AddTask.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AddTask() {
+  return (
+    <div>
+      <div>
+        <input type="checkbox" className="form-check-input" />
+        <label className="form-check-label">Check me out</label>
+      </div>
+    </div>
+  );
+}
