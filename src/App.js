fa38ad172f797ca2d87f2913f6ccd994c50c669c
Jose Padilla <jose.apadilla99@gmail.com>
2024-03-14 22:39:58 -0500

    feat: scaffold of list component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List/List';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState(["write", "read", "sleep"])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <List todos={todos}/>
       </header>
     </div>
   );
